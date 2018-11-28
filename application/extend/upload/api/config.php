ae3a053873bd6e92654316e62ed05102e108f46b
kumaomao <1083271136@qq.com>
2018-11-28 09:04:07 +0800

    1

--- application/extend/upload/api/config.php
+++ application/extend/upload/api/config.php
@@ -9,9 +9,9 @@ return [
     'Oss'=>'aliyun_oss',
     //阿里云OSS配置
     'aliyun_oss' => [
-        'KeyId'      => 'LTAIsJ5U3ZjhGKY3',  //您的Access Key ID
-        'KeySecret'  => 'GJtt7CxASmweGxZDEZjY8KKFzES6qu',  //您的Access Key Secret
-        'Endpoint'   => 'oss-cn-beijing.aliyuncs.com',  //阿里云oss 外网地址endpoint
-        'Bucket'     => 'jian-zhi',  //Bucket名称
+        'KeyId'      => '',  //您的Access Key ID
+        'KeySecret'  => '',  //您的Access Key Secret
+        'Endpoint'   => '',  //阿里云oss 外网地址endpoint
+        'Bucket'     => '',  //Bucket名称
     ],
 ];
